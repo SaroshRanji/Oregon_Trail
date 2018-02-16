f8433773a7d263b4112b9dd46146bcc696a0c77c
james-darpino <31546795+james-darpino@users.noreply.github.com>
2018-02-16 12:15:00 -0500

    Part 1,2,3 for Part 1

--- server/oregonTrail.js
+++ server/oregonTrail.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('client/public'));
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html', {root: './client/views'});
+});
 
 app.listen(1337, () => console.log('Example app listening on port 1337!'))
